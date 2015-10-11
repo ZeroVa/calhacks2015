cd5dfeade18fca034bf91ba43804567c44dc0bf6
terryttsai <iliveinabucket@berkeley.edu>
2015-10-11 03:10:51 -0400

    new top and bottom

--- imager.js
+++
@@ -1,9 +0,0 @@
-var memecanvas = require('memecanvas');
-memecanvas.init('./tmp', '-meme');
-memecanvas.generate('./photo.png', 'Top of Meme', 'Bottom of Meme', function(error, memefilename){
-    if(error){
-        console.log(error);
-    } else {
-        console.log(memefilename);
-    }
-});
--- program.js
+++ program.js
@@ -16,13 +16,26 @@ app.get('/meme/:memeText', function(req, res){
 app.get('/imager/:imageText', function(req, res) {
 	var correctedText = decodeURI(req.params.imageText);
 	var fancyText = translator.translateWords(correctedText);
-	caption.path("cat.png",{
-		caption : fancyText,
-		outputFile : "result.jpg"
-	} ,function(err,captionedImagePath){
-		console.log(captionedImagePath);
-		res.send("<img src=\"" + "http://104.236.147.66:3000/result.jpg" + "\" alt=\"Result\" >");
-	});
+
+	if (fancyText.indexOf("*") !=-1) {
+		var fancyArr = fancyText.split("*");
+		caption.path("ducreux1.jpg",{
+			caption : fancyArr[0],
+			bottomCaption : fancyArr[1],
+			outputFile : "result.jpg"
+		} ,function(err,captionedImagePath){
+			console.log(captionedImagePath);
+			res.send("<a href=\" http://niceme.me\"><img src=\"" + "http://104.236.147.66:3000/result.jpg" + "\" alt=\"Result\" ></a>");
+		});
+	} else {
+		caption.path("ducreux1.jpg",{
+			caption : fancyText,
+			outputFile : "result.jpg"
+		} ,function(err,captionedImagePath){
+			console.log(captionedImagePath);
+			res.send("<a href=\" http://niceme.me\"><img src=\"" + "http://104.236.147.66:3000/result.jpg" + "\" alt=\"Result\" ></a>");
+		});
+	}
 });
 
 app.listen(3000);
